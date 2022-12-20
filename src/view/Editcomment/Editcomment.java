2491b610e9b603599777fcbb720e85046ed4a5e6
Pujendra <lucifmorn666@gmail.com>
2022-12-20 15:59:13 +0545

    editcomment

--- src/view/Editcomment/Editcomment.java
+++ src/view/Editcomment/Editcomment.java
@@ -64,6 +64,7 @@ public class Editcomment extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        
         new Comments().setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
